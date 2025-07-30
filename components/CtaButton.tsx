430198756c4e73330db6f184e025f3827cd267ba
samuel <samuelbrito782@gmail.com>
2025-07-29 21:21:15 -0300

    fix: fix the whatsapp message

--- components/CtaButton.tsx
+++ components/CtaButton.tsx
@@ -2,7 +2,7 @@
 export default function CtaButton() {
   return (
     <a
-      href="https://wa.me/5511999999999"
+      href="https://api.whatsapp.com/send/?phone=5519989587907&text=Ol%C3%A1!%20Vi%20seu%20portf%C3%B3lio%20e%20fiquei%20muito%20interessado(a)%20no%20trabalho%20de%20voc%C3%AAs.%20Gostaria%20de%20conversar%20sobre%20a%20possibilidade%20de%20desenvolvermos%20algo%20juntos.%20Podemos%20agendar%20um%20hor%C3%A1rio%20para%20falar?&type=phone_number&app_absent=0"
       className="cursor-pointer bg-[#FFB703] hover:bg-[#e2a300] text-icarus-dark-blue font-semibold px-5 py-4 rounded"
     >
       Pronto para evoluir!
